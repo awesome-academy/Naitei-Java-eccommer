731e8de473e76e37bdfba18009046f0ec0e931db
LeKhanhChau <113273496+LeKhanhChau@users.noreply.github.com>
2023-07-31 13:41:23 +0700

    create form login (#4)
    * create form login
    
    * Sửa lại các phần review

--- ecommerce/src/main/java/app/controller/SessionController.java
+++ ecommerce/src/main/java/app/controller/SessionController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class SessionController {
 
-	@GetMapping("/")
-	public String index() {
-		return "views/user/home/index";
+	@GetMapping("/login")
+	public String login() {
+		return "views/session/login/index";
 	}
-
 }
