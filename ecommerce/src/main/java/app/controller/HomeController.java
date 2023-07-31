abe09e4f494d00921d0a828c1ddc7a52dbf61c0a
LeKhanhChau <lekhanhchau013@gmail.com>
2023-07-31 11:28:20 +0700

    create form login

--- ecommerce/src/main/java/app/controller/HomeController.java
+++ ecommerce/src/main/java/app/controller/HomeController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "views/user/home/index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "views/user/login/index";
 	}
 }
