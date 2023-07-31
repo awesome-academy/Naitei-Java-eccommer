ff03307515750f89482334cbaff54a8112c87524
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2023-07-31 13:39:27 +0700

    create view

--- ecommerce/src/main/java/app/controller/HomeController.java
+++ ecommerce/src/main/java/app/controller/HomeController.java
@@ -10,4 +10,33 @@ public class HomeController {
 	public String index() {
 		return "views/user/home/index";
 	}
+	
+	@GetMapping("/shop-grid")
+	public String shopgrid() {
+		return "views/user/shopgrid/index";
+	}
+	@GetMapping("/shop-details")
+	public String shopdetails() {
+		return "views/user/shopDetails/shopdetail";
+	}
+	@GetMapping("/shoping-cart")
+	public String shopingcart() {
+		return "views/user/shopingCart/index";
+	}
+	@GetMapping("/checkout")
+	public String checkout() {
+		return "views/user/checkOut/index";
+	}
+	@GetMapping("/blog-details")
+	public String blogdetails() {
+		return "views/user/blogDetails/index";
+	}
+	@GetMapping("/blog")
+	public String blog() {
+		return "views/user/Blog/index";
+	}
+	@GetMapping("/contact")
+	public String contact() {
+		return "views/user/Contact/index";
+	}
 }
