30e1867657735d9e2588a7177245f14bf91b2e02
= <=>
2023-08-03 09:14:16 +0700

    search category

---
+++ ecommerce/src/main/java/app/controller/SearchPageController.java
@@ -0,0 +1,27 @@
+package app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+public class SearchPageController {
+	@Controller
+	public class SearchpageController {
+
+	    @RequestMapping("/ecommerce")
+	    public String homePage() {
+	        return "views/user/home/index";
+	    }
+
+	    @RequestMapping("/ecommerce/{category}")
+	    public ResponseEntity<String> displayCategory(@PathVariable("category") String category) {
+	        
+	        String htmlContent = "<h2>You clicked on: " + category + "</h2>";
+
+	       
+	        return new ResponseEntity<>(htmlContent, HttpStatus.OK);
+	    }
+	}
+	
+}
