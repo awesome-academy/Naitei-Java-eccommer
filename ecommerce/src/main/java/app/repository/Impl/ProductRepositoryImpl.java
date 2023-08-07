4325384e074fd0935bc88f542bebcdaed612ba33
LeKhanhChau <lekhanhchau013@gmail.com>
2023-08-07 14:01:00 +0700

    show detail product

--- ecommerce/src/main/java/app/repository/Impl/ProductRepositoryImpl.java
+++ ecommerce/src/main/java/app/repository/Impl/ProductRepositoryImpl.java
@@ -3,8 +3,10 @@ package app.repository.Impl;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -23,6 +25,18 @@ public class ProductRepositoryImpl implements ProductRepository {
 			Criteria cr = session.createCriteria(Product.class);
 			List<Product> products = cr.list();
 			return products;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public Product findById(Long id) {
+		try (Session session = sessionFactory.openSession()) {
+			Criteria cr = session.createCriteria(Product.class);
+			cr.add(Restrictions.eq("id",id));
+			Product product = (Product) cr.uniqueResult();
+			return product;
 		} catch (Exception e) {
 			return null;
 		}
