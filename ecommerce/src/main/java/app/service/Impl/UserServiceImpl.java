f898e0b5330d695e08421fb365017faa87d297a3
LeKhanhChau <lekhanhchau013@gmail.com>
2023-08-17 10:53:49 +0700

    Hoàn thiện chức năng đăng ký (role_encodepassword)

--- ecommerce/src/main/java/app/service/Impl/UserServiceImpl.java
+++ ecommerce/src/main/java/app/service/Impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package app.service.Impl;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import app.model.Role;
 import app.model.User;
 import app.repository.UserRepository;
 import app.request.signUp;
@@ -17,26 +22,31 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userepository;
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public void save(signUp signup) {
 		try {
 			User user = new User();
 			user.setUsername(signup.getUsername());
-			user.setPassword(signup.getPassword());
+			user.setPassword(passwordEncoder.encode(signup.getPassword()));
 			user.setEmail(signup.getEmail());
 			user.setAddress(signup.getAddress());
 			user.setFirstName(signup.getFirstname());
 			user.setLastName(signup.getLastname());
 			user.setPhone(signup.getPhone());
+			System.out.println("User: " + user);
+			Role userRole = userepository.findRoleByName("USER");
+			Set<Role> roles = new HashSet<>();
+			roles.add(userRole);
+			user.setRoles(roles);
+			System.out.println("User: " + user);
 			userepository.save(user);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			logger.error(e);
 		}
-		
+
 	}
-	
+
 }
